182d631e2f29fdcd87208072f44807a18f727198
infinitie <infinitiecity@gmail.com>
2026-02-25 21:19:47 +0300

    Correction of the selection sort algorithm

--- python/sort.py
+++ python/sort.py
@@ -9,7 +9,7 @@ class Sort:
             for j in range(i+1, n):
                 if arr[j] < arr[min_index]:
                     min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+            arr[i], arr[min_index] = arr[min_index], arr[i]
         return arr
     
     def InsertionSort(self, arr):
@@ -28,5 +28,5 @@ class Sort:
 sort = Sort()  
 arr = [64, 56, 32, 7, 0, 23, 45, 6, 11]
 print(f"selection sort {sort.selectionSort(arr)}")
-# TODO Fix selection sort, it is not working as expected. It is not sorting the array correctly.
+print(f"Insertion sort {sort.InsertionSort(arr)}")
 #print(f"Insertion sort {sort.InsertionSort(arr)}")
