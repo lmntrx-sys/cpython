a6aee6ff4fee0e7cc2d21c8790ad8fe7f74de69b
infinitie <infinitiecity@gmail.com>
2026-02-26 20:46:13 +0300

    First Linked List

---
+++ python/LinkedList.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, val):
+        new_node = Node(val)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        current = self.head
+        while current:
+            print(current.val, end=' -> ')
+            current = current.next
+        print("None")
+
+ll = LinkedList()
+ll.insert(10)
+ll.insert(20)
+ll.insert(30)
+print("Linked List:")
+ll.printList()        
